806d4b3fedc2db417484bbdeff699e0ab3259969
azu0nyan <ZsjArgd53uziQ9R>
2019-11-11 21:00:32 +0300

    more info

--- src/ru/tutorial/other/Graphics.java
+++ src/ru/tutorial/other/Graphics.java
@@ -1,26 +1,33 @@
-package ru.tutorial.other;
+/* Максимально простой способ запустить нестатичную графику и управление с клавиатуры в своей программе.
+ * В угоду простоте ниже используются нелучшие практики программирования
+ */
+package ru.tutorial.other;//првоерьте что у вас правильно написан пакет(это должен быть путь к этому файлу от корня папки с кодом
 
-        import javax.swing.*;
-        import java.awt.*;
-        import java.awt.event.KeyAdapter;
-        import java.awt.event.KeyEvent;
-        import java.awt.event.KeyListener;
-        import java.awt.image.BufferStrategy;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
 
+/**
+ Класс - окно выводящаеся на экран благодаря extends JFrame
+ И обрабатывающее нажатия клавиш благодаря implements KeyListener
+ Название класса должно совпадать с именем файла(как и всегда)
+ */
 public class Graphics extends JFrame implements KeyListener {
-
-
     //Тут храним всякие данные
     static int x = 700;
     static int y = 700;
 
-    static  int x1Speed = 10;
+    static int x1Speed = 10;
     static int x1 = 100;
     static int y1 = 700;
 
 
-
-    //Функция рисует один кадр, в ней не должно быть никаких долгих операций и чтения из консоли
+    /**
+     * Все что должно рисоваться в вашей программе рисуется тут
+     * Функция рисует один кадр,вызывается в бесконечном цикле, не чаще 60 раз в секунду.
+     * Тут не должно быть никаких длительных операций, огромных циклов, ожидания, чтения пользовательского ввода*/
     public static void draw(Graphics2D g) {
 
         g.setColor(new Color(162, 92, 67));
@@ -32,29 +39,28 @@ public class Graphics extends JFrame implements KeyListener {
         //верхний левый угол, ширина, высота
         g.drawRect(100, 100, 300, 500);
         g.setColor(Color.RED);
-        g.fillOval(100, 100 , 300 , 500);
+        g.fillOval(100, 100, 300, 500);
 
-        g.drawLine( 400, 600, 500, 100);
+        g.drawLine(400, 600, 500, 100);
 
         /////
         g.drawOval(x - 5, y - 5, 10, 10);
 
         g.setColor(Color.GREEN);
         x1 = x1 + x1Speed;
-        if(x1 > w){
+        if (x1 > w) {
             x1Speed = -10;
-        } else if(x1 < 0){
+        } else if (x1 < 0) {
             x1Speed = 10;
         }
         g.drawOval(x1 - 5, y1 - 5, 10, 10);
     }
 
-
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_W){
+        if (e.getKeyCode() == KeyEvent.VK_W) {
             y = y - 50;
         }
-        if(e.getKeyCode() == KeyEvent.VK_S){
+        if (e.getKeyCode() == KeyEvent.VK_S) {
             y = y + 50;
         }
     }
