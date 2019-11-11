c86790dda2cb631352e957f761323b72782c2557
azu0nyan <ZsjArgd53uziQ9R>
2019-11-11 20:52:27 +0300

    graphics

---
+++ src/ru/tutorial/other/Graphics.java
@@ -0,0 +1,102 @@
+package ru.tutorial.other;
+
+        import javax.swing.*;
+        import java.awt.*;
+        import java.awt.event.KeyAdapter;
+        import java.awt.event.KeyEvent;
+        import java.awt.event.KeyListener;
+        import java.awt.image.BufferStrategy;
+
+public class Graphics extends JFrame implements KeyListener {
+
+
+    //Тут храним всякие данные
+    static int x = 700;
+    static int y = 700;
+
+    static  int x1Speed = 10;
+    static int x1 = 100;
+    static int y1 = 700;
+
+
+
+    //Функция рисует один кадр, в ней не должно быть никаких долгих операций и чтения из консоли
+    public static void draw(Graphics2D g) {
+
+        g.setColor(new Color(162, 92, 67));
+        g.setFont(new Font("Calibri", Font.BOLD, 25));
+        g.drawString("TEXT", 100, 100);//пишем текст
+
+
+        g.setStroke(new BasicStroke(4));//ширина линии
+        //верхний левый угол, ширина, высота
+        g.drawRect(100, 100, 300, 500);
+        g.setColor(Color.RED);
+        g.fillOval(100, 100 , 300 , 500);
+
+        g.drawLine( 400, 600, 500, 100);
+
+        /////
+        g.drawOval(x - 5, y - 5, 10, 10);
+
+        g.setColor(Color.GREEN);
+        x1 = x1 + x1Speed;
+        if(x1 > w){
+            x1Speed = -10;
+        } else if(x1 < 0){
+            x1Speed = 10;
+        }
+        g.drawOval(x1 - 5, y1 - 5, 10, 10);
+    }
+
+
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_W){
+            y = y - 50;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_S){
+            y = y + 50;
+        }
+    }
+
+    static final int w = 1920;
+    static final int h = 1080;
+
+    //магический код позволяющий всему работать, лучше не трогать
+    public static void main(String[] args) throws InterruptedException {
+        Graphics jf = new Graphics();
+        jf.setSize(w, h);//размер экрана
+        jf.setUndecorated(false);//показать заголовок окна
+        jf.setTitle("Моя супер программа");
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.createBufferStrategy(2);
+        jf.addKeyListener(jf);
+        //в бесконечном цикле рисуем новый кадр
+        while (true) {
+            long frameLength = 1000 / 60; //пытаемся работать из рассчета  60 кадров в секунду
+            long start = System.currentTimeMillis();
+            BufferStrategy bs = jf.getBufferStrategy();
+            Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+            g.clearRect(0, 0, jf.getWidth(), jf.getHeight());
+            draw(g);
+
+            bs.show();
+            g.dispose();
+
+            long end = System.currentTimeMillis();
+            long len = end - start;
+            if (len < frameLength) {
+                Thread.sleep(frameLength - len);
+            }
+        }
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
